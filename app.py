a9eeae5fe8d301a133c99d9eac2363f055f0540d
jorgevales <jorgevales123@gmail.com>
2023-07-09 22:01:40 -0600

    First

--- app.py
+++ app.py
@@ -10,51 +10,45 @@ from langchain.callbacks import get_openai_callback
 from dotenv import load_dotenv
 import os
 
+load_dotenv()
 
-def main():
-    load_dotenv()
+# Load the OpenAI API key from the environment variable
+if os.getenv("OPENAI_API_KEY") is None or os.getenv("OPENAI_API_KEY") == "":
+    print("OPENAI_API_KEY is not set")
+    exit(1)
+else:
+    print("OPENAI_API_KEY is set")
 
-    # Load the OpenAI API key from the environment variable
-    if os.getenv("OPENAI_API_KEY") is None or os.getenv("OPENAI_API_KEY") == "":
-        print("OPENAI_API_KEY is not set")
-        exit(1)
-    else:
-        print("OPENAI_API_KEY is set")
+st.set_page_config(page_title="EMPLOYEE DATA")
+st.header("EMPLOYEE DATA")
 
-    st.set_page_config(page_title="EMPLOYEE DATA")
-    st.header("EMPLOYEE DATA")
+csv_file = r"Data_empleados.csv"
+fixed_file = r"Data_empleados_fixed.csv"
 
-    csv_file = r"C:\Users\jorge\Langchain\Data_empleados.csv"
-    fixed_file = r"C:\Users\jorge\Langchain\Data_empleados_fixed.csv"
+# Fix accents and special characters in the CSV file
+with open(csv_file, "r", encoding="latin-1") as file:
+    content = file.read()
 
-    # Fix accents and special characters in the CSV file
-    with open(csv_file, "r", encoding="latin-1") as file:
-        content = file.read()
+with open(fixed_file, "w", encoding="utf-8") as file:
+    file.write(content)
 
-    with open(fixed_file, "w", encoding="utf-8") as file:
-        file.write(content)
+chat_model_to_use = OpenAI(temperature=0)
 
-    chat_model_to_use = OpenAI(temperature=0)
+agent = create_csv_agent(chat_model_to_use, fixed_file, verbose=True)
 
-    agent = create_csv_agent(chat_model_to_use, fixed_file, verbose=True)
+user_question = st.text_input(
+    "Hazme una pregunta del empleado que quieras conocer: "
+)
 
-    user_question = st.text_input(
-        "Hazme una pregunta del empleado que quieras conocer: "
-    )
+if user_question is not None and user_question != "":
+    with st.spinner(text="Procesando..."):
+        # Use get_openai_callback as a context manager to track token usage
+        with get_openai_callback() as cb:
+            response = agent.run(user_question)
+            st.write(response)
 
-    if user_question is not None and user_question != "":
-        with st.spinner(text="Procesando..."):
-            # Use get_openai_callback as a context manager to track token usage
-            with get_openai_callback() as cb:
-                response = agent.run(user_question)
-                st.write(response)
-
-            # Printing the token usage information
-            print(f"Total Tokens Used: {cb.total_tokens}")
-            print(f"Prompt Tokens: {cb.prompt_tokens}")
-            print(f"Completion Tokens: {cb.completion_tokens}")
-            print(f"Total Cost (USD): ${format(cb.total_cost, '.50f')}")
-
-
-if __name__ == "__main__":
-    main()
+        # Printing the token usage information
+        print(f"Total Tokens Used: {cb.total_tokens}")
+        print(f"Prompt Tokens: {cb.prompt_tokens}")
+        print(f"Completion Tokens: {cb.completion_tokens}")
+        print(f"Total Cost (USD): ${format(cb.total_cost, '.50f')}")
